ac78770b6a7ec607e9205d5d039ee30523e851c8
vishwanath191 <92415871+vishwanath191@users.noreply.github.com>
2024-03-05 01:49:50 +0530

    Update faculty management.c

--- faculty management.c
+++ faculty management.c
@@ -126,3 +126,40 @@ boolean found = false;
         }
     }
 }
+class Faculty {
+    private int id;
+    private String name;
+    private int experience;
+    private double salary;
+
+    public Faculty(int id, String name, int experience, double salary) {
+        this.id = id;
+        this.name = name;
+        this.experience = experience;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void displayFaculty() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Experience: " + experience + " years");
+        System.out.println("Salary: $" + salary);
+        System.out.println("--------------------------");
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
